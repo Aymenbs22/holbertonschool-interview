3facb1f34fae38ec85292dce33c7f63b9c3f1fbc
Aymenbs22 <aymenbenslima1@gmail.com>
2023-04-06 21:59:06 +0100

    function that creates a binary tree node:
    Prototype: binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
    parent is a pointer to the parent node of the node to create
    value is the value to put in the new node
    When created, a node does not have any children
    Your function must return a pointer to the new node, or NULL on failure

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 /**
- * struct binary_tree_node - function that creates a binary tree node
+ * binary_tree_node - function that creates a binary tree node
  * @value: is the value to put in the new node
  * @parent: Pointer to the parent node
+ * Return: new_node
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-
-    new_node = malloc(sizeof(binary_tree_t));
-    
-    if (new_node == NULL){
-        return (NULL);
-    }
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    return (new_node);
+binary_tree_t *new_node;
+new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+return (new_node);
 }
