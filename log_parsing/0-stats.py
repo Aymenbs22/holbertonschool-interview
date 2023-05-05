13f09ecc879795bf44742eb0a6cd823b3c7eb6f5
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-05 20:30:46 +0100

    script that reads stdin line by line and computes metrics: Input format: <IP Address> - [<date>] GET /projects/260 HTTP/1.1 <status code> <file size> (if the format is not this one, the line must be skipped) After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning: Total file size: File size: <total size> where <total size> is the sum of all previous <file size> (see input format above) Number of lines by status code: possible status code: 200, 301, 400, 401, 403, 404, 405 and 500 if a status code doesn’t appear or is not an integer, don’t print anything for this status code format: <status code>: <number> status codes should be printed in ascending order Warning: In this sample, you will have random value - it’s normal to not have the same output as this one

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
-'''
-script that reads stdin line by line and computes metrics
-'''
 import sys
 
-total_size = 0
-status_codes = []
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+totalsize = 0
+list = []
 
-for line in sys.stdin:
-    fields = line.strip().split()
-    status_code = fields[-2]
-    size = int(fields[-1])
+try:
+    for line in sys.stdin:
+        fields = line.split()
+        status_code = int(fields[-2])
+        totalsize += int(fields[-1])
+        list.append(status_code)
+        list.sort()
 
-    total_size += size
-    status_codes.append(status_code)
-
-    if len(status_codes) == 10:
-        print("File size:", total_size)
-        print('\n'.join(sorted(status_codes)))
-        status_codes = []
+        if (len(list) % 10) == 0:
+            print("File size:", totalsize)
+            count = 1
+            status = 0
+            for i in list:
+                if int(i) not in status_codes:
+                    continue
+                elif status == 0:
+                    status = i
+                elif status != i:
+                    print("{}: {}".format(status, count))
+                    status = i
+                    count = 1
+                else:
+                    count += 1
+            if int(status) in status_codes:
+                print("{}: {}".format(status, count))
+except Exception:
+    pass
