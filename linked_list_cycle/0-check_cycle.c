a7db5dfb8611052b079f9bf28c28f487a4e19a0c
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-12 18:25:34 +0100

    function in C that checks if a singly linked list has a cycle in it.
    Prototype: int check_cycle(listint_t *list);
    Return: 0 if there is no cycle, 1 if there is a cycle

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * check_cycle - function in C that checks if a singly linked list has a cycle in it
+ * @list: list
+ * @return: return
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *onestep = list;
+	listint_t *twostep = list;
+
+	while (onestep && twostep != NULL)
+	{
+		onestep = onestep->next;
+		twostep = twostep->next->next;
+		if (onestep == twostep)
+			return(1);
+}
+	return(0);
+}
+
