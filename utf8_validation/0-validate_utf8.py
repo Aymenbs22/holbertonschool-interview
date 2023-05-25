8d6d73a7e04f644631ed4d57fd9bdefb375ea310
Aymenbs22 <aymenbenslima0@gmail.com>
2023-05-25 16:45:31 +0100

    method that determines if a given data set represents a valid UTF-8 encoding.
    Prototype: def validUTF8(data)
    Return: True if data is a valid UTF-8 encoding, else return False
    A character in UTF-8 can be 1 to 4 bytes long
    The data set can contain multiple characters
    The data will be represented by a list of integers
    Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""method that determines if a given data set represents a valid UTF-8 encoding.
+"""method that determines if a given data
+set represents a valid UTF-8 encoding.
 Prototype: def validUTF8(data)
 Return: True if data is a valid UTF-8 encoding, else return False
 A character in UTF-8 can be 1 to 4 bytes long
@@ -8,26 +9,31 @@ The data will be represented by a list of integers
 Each integer represents 1 byte of data, therefore you only
 need to handle the 8 least significant bits of each integer"""
 
+
 def validUTF8(data):
-        rem = 0
-        for i in data:
-            if rem == 0:
-                if i >> 7 == 0b0:
-                    rem = 0
-                elif i >> 5 == 0b110:
-                    rem = 1
-                elif i >> 4 == 0b1110:
-                    rem = 2
-                elif i >> 3 == 0b11110:
-                    rem = 3
-                else:
-                    return False
-            else:
-                if i >> 6 == 0b10:
-                    rem -= 1
-                else:
-                    return False
+    """
+    method that determines if a given data
+set represents a valid UTF-8 encoding
+    """
+    rem = 0
+    for i in data:
         if rem == 0:
-            return True
+            if i >> 7 == 0b0:
+                rem = 0
+            elif i >> 5 == 0b110:
+                rem = 1
+            elif i >> 4 == 0b1110:
+                rem = 2
+            elif i >> 3 == 0b11110:
+                rem = 3
+            else:
+                return False
         else:
-            return False
+            if i >> 6 == 0b10:
+                rem -= 1
+            else:
+                return False
+    if rem == 0:
+        return True
+    else:
+        return False
