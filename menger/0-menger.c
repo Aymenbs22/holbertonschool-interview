f731d8ee619ce76457f3baa617586676c84e8e8b
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-07 20:45:35 +0100

    function that draws a 2D Menger Sponge
    Prototype: void menger(int level);
    Where level is the level of the Menger Sponge to draw
    If level is lower than 0, your function must do nothing
    You’re allowed to use the math library. Your program will be compiled using the ld flag -lm
    Format:
    
    First, read Menger sponge
    Here, we will only draw a 2D version of the Menger sponge, but the principle is the same
    A level N sponge is a 3x3 square of level N-1 sponges, except for the center one, which is left empty
    A level 0 sponge is represented by the # character
    Examples:
    A level 0 sponge is a simple 1x1 square
    A level 1 sponge is a 3x3 square of level 0 sponges, except for the center one, which is left empty
    A level 2 sponge is a 3x3 square of level 1 sponges, except for the center one, which is left empty
    …
    TIP: The size of a level N Menger sponge is calculated as follows: 3^N

---
+++ menger/0-menger.c
@@ -0,0 +1,42 @@
+#include "menger.h"
+/**
+ * menger - function that draws a 2D Menger Sponge
+ * @level: level is the level of the Menger Sponge to draw
+ * Return: void
+ */
+
+void menger(int level)
+{
+size_t size;
+size_t i;
+size_t j;
+
+if (level < 0)
+{
+return;
+}
+size = pow(3, level);
+for (j = 0; j < size; j++)
+{
+for (i = 0; i < size; i++)
+{
+bool b = false;
+size_t tmp1 = j;
+size_t tmp2 = i;
+while (tmp1 && tmp2)
+{
+if (tmp1 % 3 == 1 && tmp2 % 3 == 1)
+{
+b = true;
+}
+tmp1 /= 3;
+tmp2 /= 3;
+}
+if (b == true)
+printf(" ");
+else 
+printf("#");
+}
+printf("\n");
+}
+}
