83ea95f901c00dfc47fb017fffe4ccd45166b939
Aymenbs22 <aymenbenslima0@gmail.com>
2023-06-07 18:30:48 +0100

    Sandpiles sum

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -10,6 +10,14 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    if (grid1 == NULL)
-    if (grid2 == NULL)
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
 }
